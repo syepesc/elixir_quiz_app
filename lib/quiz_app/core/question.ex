71caf47bf0b7ffe4bbe8b839711d7acdd508465a
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-02-17 18:54:56 -0500

    Build your functional core

--- lib/quiz_app/core/question.ex
+++ lib/quiz_app/core/question.ex
@@ -8,5 +8,39 @@ defmodule QuizApp.Core.Question do
   - `substitutions`(%{substitution: any}): The values chosen for each substitution field in a template.
   """
 
+  alias QuizApp.Core.Template
+
   defstruct [:asked, :template, :substitutions]
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp compile(%Template{} = template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      template: template,
+      substitutions: substitutions
+    }
+  end
+
+  defp build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
 end
--- lib/quiz_app/core/template.ex
+++ lib/quiz_app/core/template.ex
@@ -9,10 +9,19 @@ defmodule QuizApp.Core.Template do
   - `raw`(string): The raw template code before compilation.
   - `compiled`(macro): The compiled template for execution.
   - `generators`(%{substitution: list | function}): The generator for each
-    substitution in a tempalte. Each is a list of elements or a function.
+    substitution in a tempalte. Generates a question set questions in a list.
     Generating a tempalte substitution will either fire the function or pick a random irem from the list.
   - `checker`(function(substitutions, string) -> boolean): The function that checks if a given answer is correct.
   """
 
   defstruct [:name, :category, :instructions, :raw, :compiled, :generators, :checker]
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+
+    struct!(
+      __MODULE__,
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    )
+  end
 end
