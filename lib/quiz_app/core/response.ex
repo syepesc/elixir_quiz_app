96b050017ef5cae796a1dc8f3daa8b0d31efc48c
Santiago Yepes <santiagoyepescarrera@gmail.com>
2025-02-17 20:05:20 +0000

    Build your functional core

--- lib/quiz_app/core/response.ex
+++ lib/quiz_app/core/response.ex
@@ -12,5 +12,22 @@ defmodule QuizApp.Core.Response do
   - `timestamp`(DateTime): The time the response was submitted.
   """
 
+  alias QuizApp.Core.Quiz
+
   defstruct [:quiz_title, :template_name, :to, :email, :answer, :correct, :timestamp]
+
+  def new(%Quiz{} = quiz, email, answer) do
+    question = quiz.current_question
+    template = question.template
+
+    %__MODULE__{
+      quiz_title: quiz.title,
+      template_name: template.name,
+      to: question.asked,
+      email: email,
+      answer: answer,
+      correct: template.checker.(question.substitutions, answer),
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
